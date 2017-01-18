9b5ba4e47a374a67a3982872687c7746631baa23
Shardul Patel <shardul@codigami.com>
2017-01-18 19:45:49 +0530

    1. Swift Bridging Header Added 2. CFAlertAction swift Created

---
+++ CFAlertViewController/Models/CFAlertAction.swift
@@ -0,0 +1,13 @@
+//
+//  CFAlertAction.swift
+//  CFAlertViewControllerDemo
+//
+//  Created by Shardul Patel on 18/01/17.
+//  Copyright © 2017 Codigami Inc. All rights reserved.
+//
+
+import UIKit
+
+class CFAlertAction: NSObject {
+
+}

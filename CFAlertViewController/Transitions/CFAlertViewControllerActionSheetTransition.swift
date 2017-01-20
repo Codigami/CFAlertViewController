ed08660c758a96cf8e2e93c1938a0bc291ad01bb
Shivam Bhalla <shivam@Shivams-Work-MacBook-Pro.local>
2017-01-20 13:19:20 +0530

    Create swift files for transitions

---
+++ CFAlertViewController/Transitions/CFAlertViewControllerActionSheetTransition.swift
@@ -0,0 +1,13 @@
+////
+////  CFAlertViewControllerActionSheetTransition.swift
+////  CFAlertViewControllerDemo
+////
+////  Created by Shivam Bhalla on 1/20/17.
+////  Copyright © 2017 Codigami Inc. All rights reserved.
+////
+//
+//import UIKit
+//
+//class CFAlertViewControllerActionSheetTransition: NSObject {
+//
+//}
---
+++ CFAlertViewController/Transitions/CFAlertViewControllerPopupTransition.swift
@@ -0,0 +1,13 @@
+////
+////  CFAlertViewControllerPopupTransition.swift
+////  CFAlertViewControllerDemo
+////
+////  Created by Shivam Bhalla on 1/20/17.
+////  Copyright © 2017 Codigami Inc. All rights reserved.
+////
+//
+//import UIKit
+//
+//class CFAlertViewControllerPopupTransition: NSObject {
+//
+//}
